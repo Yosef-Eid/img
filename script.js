b65efebe6e781c2df77afe52ebff6818a03d43b5
Yousef-Eid <117477110+Yosef-Eid@users.noreply.github.com>
2023-03-29 03:05:27 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,138 @@
+
+
+let img0 = document.getElementById("img0")
+img0.addEventListener("click" , ()=>{
+    img0.style.width = "600px"
+    img0.style.height = "600px"
+    img0.style.zIndex = "3"
+})
+img0.addEventListener("mouseout" , ()=>{
+    img0.style.width = "200px"
+    img0.style.height = "200px"
+    img0.style.zIndex = "0"
+})
+
+
+let img1 = document.getElementById("img1")
+img1.addEventListener("click" , ()=>{
+    img1.style.width = "600px"
+    img1.style.height = "600px"
+    img1.style.left = "0px"
+    img1.style.zIndex = "3"
+})
+img1.addEventListener("mouseout" , ()=>{
+    img1.style.width = "200px"
+    img1.style.height = "200px"
+    img1.style.left = "200px"
+    img1.style.zIndex = "0"
+})
+
+let img2 = document.getElementById("img2")
+img2.addEventListener("click" , ()=>{
+    img2.style.width = "600px"
+    img2.style.height = "600px"
+    img2.style.right = "0px"
+    img2.style.zIndex = "3"
+})
+img2.addEventListener("mouseout" , ()=>{
+    img2.style.width = "200px"
+    img2.style.height = "200px"
+    img2.style.right = "0px"
+    img2.style.zIndex = "0"
+})
+
+let img3 = document.getElementById("img3")
+img3.addEventListener("click" , ()=>{
+    img3.style.width = "600px"
+    img3.style.height = "600px"
+    img3.style.left = "0px"
+    img3.style.top = "0px"
+    img3.style.zIndex = "3"
+})
+img3.addEventListener("mouseout" , ()=>{
+    img3.style.width = "200px"
+    img3.style.height = "200px"
+    img3.style.left = "0px"
+    img3.style.top = "200px"
+    img3.style.zIndex = "0"
+})
+
+let img4 = document.getElementById("img4")
+img4.addEventListener("click" , ()=>{
+    img4.style.width = "600px"
+    img4.style.height = "600px"
+    img4.style.left = "0px"
+    img4.style.top = "0px"
+    img4.style.zIndex = "3"
+})
+img4.addEventListener("mouseout" , ()=>{
+    img4.style.width = "200px"
+    img4.style.height = "200px"
+    img4.style.left = "200px"
+    img4.style.top = "200px"
+    img4.style.zIndex = "0"
+})
+
+let img5 = document.getElementById("img5")
+img5.addEventListener("click" , ()=>{
+    img5.style.width = "600px"
+    img5.style.height = "600px"
+    img5.style.right = "0px"
+    img5.style.top = "0px"
+    img5.style.zIndex = "3"
+})
+img5.addEventListener("mouseout" , ()=>{
+    img5.style.width = "200px"
+    img5.style.height = "200px"
+    img5.style.right = "0px"
+    img5.style.top = "200px"
+    img5.style.zIndex = "0"
+})
+
+let img6 = document.getElementById("img6")
+img6.addEventListener("click" , ()=>{
+    img6.style.width = "600px"
+    img6.style.height = "600px"
+    img6.style.left = "0px"
+    img6.style.top = "0px"
+    img6.style.zIndex = "3"
+})
+img6.addEventListener("mouseout" , ()=>{
+    img6.style.width = "200px"
+    img6.style.height = "200px"
+    img6.style.left = "0px"
+    img6.style.top = "400px"
+    img6.style.zIndex = "0"
+})
+
+let img7 = document.getElementById("img7")
+img7.addEventListener("click" , ()=>{
+    img7.style.width = "600px"
+    img7.style.height = "600px"
+    img7.style.left = "0px"
+    img7.style.top = "0px"
+    img7.style.zIndex = "3"
+})
+img7.addEventListener("mouseout" , ()=>{
+    img7.style.width = "200px"
+    img7.style.height = "200px"
+    img7.style.left = "200px"
+    img7.style.top = "400px"
+    img7.style.zIndex = "0"
+})
+
+let img8 = document.getElementById("img8")
+img8.addEventListener("click" , ()=>{
+    img8.style.width = "600px"
+    img8.style.height = "600px"
+    img8.style.right = "0px"
+    img8.style.top = "0px"
+    img8.style.zIndex = "3"
+})
+img8.addEventListener("mouseout" , ()=>{
+    img8.style.width = "200px"
+    img8.style.height = "200px"
+    img8.style.right = "0px"
+    img8.style.top = "400px"
+    img8.style.zIndex = "0"
+})
